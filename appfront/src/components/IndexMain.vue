836d1f6dbe5b585df4b45fa0876e29b3d6b71e3d
lcs1998 <lcs1998@vip.qq.com>
2019-01-18 09:38:18 +0800

    登录界面

--- appfront/src/components/IndexMain.vue
+++ appfront/src/components/IndexMain.vue
@@ -1,68 +1,463 @@
 /* eslint-disable */
 <template>
-  <el-row class="tac">
-    <el-col :span="12">
-      <el-menu
-        default-active="2"
-        class="el-menu-vertical-demo"
-        @open="handleOpen"
-        @close="handleClose"
-        background-color="#545c64"
-        text-color="#fff"
-        active-text-color="#ffd04b">
-        <el-submenu index="1">
-          <template slot="title">
-            <i class="el-icon-location"></i>
-            <span>导航一</span>
-          </template>
-          <el-menu-item-group>
-            <template slot="title">分组一</template>
-            <el-menu-item index="1-1">选项1</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="1-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="1-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="1-4-1">选项1</el-menu-item>
-          </el-submenu>
-        </el-submenu>
-        <el-menu-item index="2">
-          <i class="el-icon-menu"></i>
-          <span slot="title">导航二</span>
-        </el-menu-item>
-        <el-menu-item index="3" disabled>
-          <i class="el-icon-document"></i>
-          <span slot="title">导航三</span>
-        </el-menu-item>
-        <el-menu-item index="4">
-          <i class="el-icon-setting"></i>
-          <span slot="title">导航四</span>
-        </el-menu-item>
-      </el-menu>
-    </el-col>
-  </el-row>
+  <div id="bg">
+    <div id="hint"><!-- 提示框 -->
+      <p>登录失败</p>
+    </div>
+    <div id="login_wrap">
+      <div id="login"><!-- 登录注册切换动画 -->
+        <div class="denglu">
+          <i style="top: 0">Log</i>
+          <i style="right: 5px">in</i>
+        </div>
+        <span>
+                    <form action="post">
+                        <p class="form"><input type="text" id="user" placeholder="username"></p>
+                        <p class="form"><input type="password" id="passwd" placeholder="password"></p>
+                        <p class="form confirm"><input type="password" id="confirm-passwd"
+                                                       placeholder="confirm password"></p>
+                        <input type="button" value="Log in" class="btn" onclick="login()" style="margin-right: 20px;">
+                    </form>
+                </span>
+      </div>
+
+      <div id="login_img"><!-- 图片绘制框 -->
+        <span class="circle">
+                    <span></span>
+                    <span></span>
+                </span>
+        <span class="star">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </span>
+        <span class="fly_star">
+                    <span></span>
+                    <span></span>
+                </span>
+        <p id="title">教 务 管 理 系 统</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 /* eslint-disable */
   export default {
-      name: "IndexMain",
-      data () {
-
+    name: "IndexMain",
+    data() {
+      return {
+        input_name: '',
+        input_pass: '',
+        onoff: false,
+        login: true,
+        confirm: document.getElementsByClassName("confirm")[0],
+        name: name_c.innerHTML.split(""),
+        name_c: document.getElementById("title"),
+        user: document.getElementById("user"),
+        passwd: document.getElementById("passwd"),
+        con_pass: document.getElementById("confirm-passwd")
+      }
+    },
+    methods: {
+      //引用hint()在最上方弹出提示
+      hint() {
+        let hit = document.getElementById("hint")
+        hit.style.display = "block"
+        setTimeout("hit.style.opacity = 1", 0)
+        setTimeout("hit.style.opacity = 0", 2000)
+        setTimeout('hit.style.display = "none"', 3000)
       },
-      methods: {
-        handleOpen(key, keyPath) {
-          console.log(key, keyPath);
-        },
-        handleClose(key, keyPath) {
-          console.log(key, keyPath);
+      //回调函数
+      submit(callback) {
+        if (passwd.value == con_pass.value) {
+          let request = new XMLHttpRequest()
+          let url = ""
+          request.open("post", url, true)
+          let data = new FormData()
+          data.append("user", user.value)
+          data.append("passwd", passwd.value)
+          request.onreadystatechange = function () {
+            if (this.readyState == 4) {
+              callback.call(this, this.response)
+              //console.log(this.responseText)
+            }
+          }
+          request.send(data)
+        } else {
+          hit.innerHTML = "两次密码不同"
+          hitting()
         }
+      },
+      //登录按钮
+      login() {
+        if (this.onoff) {
+          let request = new XMLHttpRequest()
+          let url = ""
+          request.open("post", url, true)
+          let data = new FormData()
+          data.append("user", user.value)
+          data.append("passwd", passwd.value)
+          request.onreadystatechange = function () {
+            if (this.readyState == 4) {
+              if (this.responseText == false)
+                hint()
+              else
+                window.location.href = this.responseText;
+            }
+          }
+          request.send(data)
+        } else {
+          let status = document.getElementById("status").getElementsByTagName("i")
+          confirm.style.height = 0
+          status[0].style.top = 0
+          status[1].style.top = 35 + "px"
+          this.onoff = !this.onoff
+        }
+      },
+      init() {
+        //自动居中title
+        name_c.innerHTML = ""
+        for (i = 0; i < name.length; i++)
+          if (name[i] != ",")
+            name_c.innerHTML += "<i>" + name[i] + "</i>"
+      },
+      mounted(){
+        this.init();
       }
     }
+  }
+
+
 </script>
 
 <style scoped>
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  .denglu{
+    margin-top: 50px;
+    font-family: "neo";
+    font-size: 25px;
+    color: white;
+  }
+
+  html,
+  body {
+    height: 100%;
+  }
+
+  @font-face {
+    font-family: 'neo';
+    src: url("../assets/font/NEOTERICc.ttf");
+  }
+
+  .bg{
+    height: 100%;
+  }
+
+  input:focus {
+    outline: none;
+  }
+
+  .form input {
+    width: 300px;
+    height: 30px;
+    font-size: 18px;
+    background: none;
+    border: none;
+    border-bottom: 1px solid #fff;
+    color: #fff;
+    margin-bottom: 20px;
+  }
+
+  .form input::placeholder {
+    color: rgba(255, 255, 255, 0.8);
+    font-size: 18px;
+    font-family: "neo";
+  }
+
+  .confirm {
+    height: 0;
+    overflow: hidden;
+    transition: .25s;
+  }
+
+  .btn {
+    width: 140px;
+    height: 40px;
+    border: 1px solid #fff;
+    background: none;
+    font-size: 20px;
+    color: #fff;
+    cursor: pointer;
+    margin-top: 25px;
+    font-family: "neo";
+    transition: .25s;
+  }
+
+  .btn:hover {
+    background: rgba(255, 255, 255, .25);
+  }
+
+  #login_wrap {
+    width: 980px;
+    min-height: 500px;
+    border-radius: 10px;
+    font-family: "neo";
+    overflow: hidden;
+    box-shadow: 0px 0px 120px rgba(0, 0, 0, 0.25);
+    position: fixed;
+    top: 50%;
+    right: 50%;
+    margin-top: -250px;
+    margin-right: -490px;
+  }
+
+  #login {
+    width: 50%;
+    height: 100%;
+    min-height: 500px;
+    background: linear-gradient(45deg, #9a444e, #e06267);
+    position: relative;
+    float: right;
+  }
+
+  #login #status {
+    width: 90px;
+    height: 35px;
+    margin: 40px auto;
+    color: #fff;
+    font-size: 30px;
+    font-weight: 600;
+    position: relative;
+    overflow: hidden;
+  }
+
+  #login #status i {
+    font-style: normal;
+    position: absolute;
+    transition: .5s
+  }
+
+  #login span {
+    text-align: center;
+    position: absolute;
+    left: 50%;
+    margin-left: -150px;
+    top: 52%;
+    margin-top: -140px;
+  }
+
+  #login span a {
+    text-decoration: none;
+    color: #fff;
+    display: block;
+    margin-top: 80px;
+    font-size: 18px;
+  }
+
+  /*绘图*/
+  #login_img {
+    width: 50%;
+    min-height: 500px;
+    background: linear-gradient(45deg, #221334, #6c3049);
+    float: left;
+    position: relative;
+  }
+
+  #login_img span {
+    position: absolute;
+    display: block;
+  }
+
+  #login_img .circle {
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    background: linear-gradient(45deg, #df5555, #ef907a);
+    top: 70px;
+    left: 50%;
+    margin-left: -100px;
+    overflow: hidden;
+  }
+
+  #login_img .circle span {
+    width: 150px;
+    height: 40px;
+    border-radius: 50px;
+    position: absolute;
+  }
+
+  #login_img .circle span:nth-child(1) {
+    top: 30px;
+    left: -38px;
+    background: #c55c59;
+  }
+
+  #login_img .circle span:nth-child(2) {
+    bottom: 20px;
+    right: -35px;
+    background: #934555;
+  }
+
+  #login_img .star span {
+    background: radial-gradient(#fff 10%, #fff 20%, rgba(72, 34, 64, 0));
+    border-radius: 50%;
+    box-shadow: 0 0 7px #fff;
+  }
+
+  #login_img .star span:nth-child(1) {
+    width: 15px;
+    height: 15px;
+    top: 50px;
+    left: 30px;
+  }
+
+  #login_img .star span:nth-child(2) {
+    width: 10px;
+    height: 10px;
+    left: 360px;
+    top: 80px;
+  }
+
+  #login_img .star span:nth-child(3) {
+    width: 5px;
+    height: 5px;
+    top: 400px;
+    left: 80px;
+  }
+
+  #login_img .star span:nth-child(4) {
+    width: 8px;
+    height: 8px;
+    top: 240px;
+    left: 60px;
+  }
+
+  #login_img .star span:nth-child(5) {
+    width: 4px;
+    height: 4px;
+    top: 20px;
+    left: 200px;
+  }
+
+  #login_img .star span:nth-child(6) {
+    width: 4px;
+    height: 4px;
+    top: 460px;
+    left: 410px;
+  }
+
+  #login_img .star span:nth-child(7) {
+    width: 6px;
+    height: 6px;
+    top: 250px;
+    left: 350px;
+  }
+
+  #login_img .fly_star span {
+    width: 90px;
+    height: 3px;
+    background: -webkit-linear-gradient(left, rgba(255, 255, 255, 0.67), rgba(255, 255, 255, 0));
+    background: -o-linear-gradient(left, rgba(255, 255, 255, 0.67), rgba(255, 255, 255, 0));
+    background: linear-gradient(to right, rgba(255, 255, 255, 0.67), rgba(255, 255, 255, 0));
+    transform: rotate(-45deg);
+  }
+
+  #login_img .fly_star span:nth-child(1) {
+    top: 60px;
+    left: 80px;
+  }
+
+  #login_img .fly_star span:nth-child(2) {
+    top: 210px;
+    left: 332px;
+    opacity: 0.6;
+  }
+
+  #login_img p {
+    text-align: center;
+    color: #af4b55;
+    font-weight: 600;
+    margin-top: 365px;
+    font-size: 25px;
+  }
+
+  #login_img p i {
+    font-style: normal;
+    margin-right: 45px;
+  }
+
+  #login_img p i:nth-last-child(1) {
+    margin-right: 0;
+  }
+
+  /*提示*/
+  #hint {
+    width: 100%;
+    line-height: 70px;
+    background: linear-gradient(-90deg, #9b494d, #bf5853);
+    text-align: center;
+    font-size: 25px;
+    color: #fff;
+    box-shadow: 0 0 20px #733544;
+    display: none;
+    opacity: 0;
+    transition: .5s;
+    position: absolute;
+    top: 0;
+    z-index: 999;
+  }
+
+  /* 响应式 */
+  @media screen and (max-width: 1000px ) {
+    #login_img {
+      display: none;
+    }
+
+    #login_wrap {
+      width: 490px;
+      margin-right: -245px;
+    }
+
+    #login {
+      width: 100%;
+
+    }
+  }
+
+  @media screen and (max-width: 560px ) {
+    #login_wrap {
+      width: 330px;
+      margin-right: -165px;
+    }
+
+    #login span {
+      margin-left: -125px;
+    }
+
+    .form input {
+      width: 250px;
+    }
+
+    .btn {
+      width: 113px;
+    }
+  }
+
+  @media screen and (max-width: 345px ) {
+    #login_wrap {
+      width: 290px;
+      margin-right: -145px;
+    }
+  }
 
 </style>
